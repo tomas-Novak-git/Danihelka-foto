849f5d50d21a8f388a60f3c33734c7fb0ae71501
Yatrikoxi <tomas_novak@centrum.cz>
2024-01-03 14:47:12 +0100

    nav-scrollCH,SVGscrollCH

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 const header = document.querySelector("#nav-bar");
+const svgOne = document.querySelector("#svg-icon1")
+const svgTwo = document.querySelector("#svg-icon2")
+const images = document.querySelector(".icons")
 const sectionOne = document.querySelector(".hero");
 
 const sectionOneOptions= {
-    rootMargin: "-110px 0px 0px 0px"
+    rootMargin: "-175px 0px 0px 0px"
 };
 
 const sectionOneObserver = new IntersectionObserver(function(entries, sectionOneObserver){
     entries.forEach(entry => {
         if(!entry.isIntersecting) {
             header.classList.add('nav-scrolled');
+            svgOne.classList.remove("icon-white")
+            svgTwo.classList.remove("icon-white")
         } else {
             header.classList.remove("nav-scrolled");
+            svgOne.classList.add("icon-white");
+            svgTwo.classList.add("icon-white");
         }
     })
 }, sectionOneOptions);
